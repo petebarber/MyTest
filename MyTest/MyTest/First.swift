c246cbf6cf32914d9d78f03467570c78d2599ad3
pete <pete.barber@gmail.com>
2015-06-05 21:20:49 +0100

    Setup done

---
+++ MyTest/MyTest/First.swift
@@ -0,0 +1,9 @@
+//
+//  First.swift
+//  MyTest
+//
+//  Created by Pete on 05/06/2015.
+//  Copyright (c) 2015 MyTest. All rights reserved.
+//
+
+import Foundation
---
+++ MyTest/MyTest/MyTest.h
@@ -0,0 +1,19 @@
+//
+//  MyTest.h
+//  MyTest
+//
+//  Created by Pete on 05/06/2015.
+//  Copyright (c) 2015 MyTest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyTest.
+FOUNDATION_EXPORT double MyTestVersionNumber;
+
+//! Project version string for MyTest.
+FOUNDATION_EXPORT const unsigned char MyTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyTest/PublicHeader.h>
+
+

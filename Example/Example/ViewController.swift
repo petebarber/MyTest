95713ebbd97599f2bb144df337ddefa6c76fba78
pete <pete.barber@gmail.com>
2015-06-05 21:21:29 +0100

    Oh well, might as well do it all

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MyTest
 
 class ViewController: UIViewController {
 
--- MyTest/MyTest/First.swift
+++ MyTest/MyTest/First.swift
@@ -5,5 +5,12 @@
 //  Created by Pete on 05/06/2015.
 //  Copyright (c) 2015 MyTest. All rights reserved.
 //
-
 import Foundation
+
+public class First
+{
+	public func f() -> String
+	{
+		return "Hello, world"
+	}
+}
